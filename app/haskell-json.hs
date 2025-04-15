8b7f70aeff04180038ed3609c196904b2a332505
Gabriel Le Dain <153308331+g-ledain@users.noreply.github.com>
2025-04-16 00:24:40 +0100

    Finish definition of epsilon closure

--- app/haskell-json.hs
+++ app/haskell-json.hs
@@ -1,6 +1,8 @@
 import Data.Map (Map)
 import qualified Data.Set.Monad as Set--implements monad on Set
 import Control.Applicative
+import Control.Monad
+import Data.List
 
 --Set.Monad doesn't export cartesianProduct, so we'll have to do it ourselves
 --Definition taken from https://hackage.haskell.org/package/containers-0.8/docs/Data-Set.html#v:cartesianProduct
@@ -36,23 +38,27 @@ shiftEnd xs = append Nothing (fmap Just xs)
 consecutivePairs :: [a] -> [(Maybe a, Maybe a)]
 consecutivePairs xs = zip (shiftFront xs) (shiftEnd xs)
 
-compute :: (Ord states) => StateMachine states alphabet -> [alphabet] -> states
-compute (StateMachine transition initial _accept)  = foldr transition initial
-
 oneStep :: (Ord states) => StateMachine states alphabet -> (alphabet, (Maybe states, Maybe states)) -> Bool
 oneStep (StateMachine _t  initial _acceptSet) (_letter, (Nothing, Just state)) = state == initial
 oneStep (StateMachine t _initial _acceptSet) (letter, (Just s1, Just s2)) = t letter s1 == s2
 oneStep (StateMachine _t _initial acceptSet) (_letter,  (Just state, Nothing)) = Set.member state acceptSet
-oneStep (StateMachine _t _initial _acceptSet)  (_letter, (Nothing, Nothing)) = False
+oneStep (StateMachine _t _initial _acceptSet)  (_letter, (Nothing, Nothing)) = False--should never happen in our use of this function
 
 --checks whether the given sequence of states exhibits the computation of the state machine on the given word
 exhibit :: (Ord states) => StateMachine states alphabet -> [alphabet] -> [states] -> Bool
 exhibit machine word states = all (oneStep machine) transitionPairs where
     transitionPairs = zip word (consecutivePairs states)
 
+compute :: (Ord states) => StateMachine states alphabet -> [alphabet] -> states
+compute (StateMachine transition initial _accept)  = foldr transition initial
 
 accept :: (Ord states) => StateMachine states alphabet -> [alphabet] -> Bool
 accept (StateMachine t i a) word = Set.member (compute (StateMachine t i a) word) a
+
+
+-- we should have the following laws:
+-- exhibit machine word states == accept machine word
+-- exhibit (StateMachine transition initial acceptSet) word states == Set.Member (compute (StateMachine transition initial acceptSet) word) acceptSet
 
 stateProduct :: (Ord states1, Ord states2) => StateMachine states1 alphabet -> StateMachine states2 alphabet -> StateMachine (states1, states2) alphabet
 stateProduct (StateMachine t1 i1 a1) (StateMachine t2 i2 a2) = StateMachine transition (i1, i2) (cartesianProduct a1 a2) where
@@ -84,16 +90,21 @@ data EpsilonNDStateMachine states alphabet = EpsilonNDStateMachine {
     epsilonNdAcceptStates :: Set.Set states
 }
 
+stabiliseList :: (Eq b) => (b -> [b]) -> b -> [b]
+stabiliseList func b = join (stabiliseFunc (join . fmap func ) [b])
+
+stabiliseFunc :: (Eq a) => (a -> a) -> a -> [a]
+stabiliseFunc f x = unfoldr (\ y -> if f y == y then Nothing else Just (f y, y)) x
+
 --iterate until output stabilises
+--this is a horrible definition - should be able to do this "natively" on lists
+--I've not got a good handle on how to generalise unfold yet...
 stabilise :: (Ord b) => (b -> Set.Set b) -> b -> Set.Set b
-stabilise b bs = Set.empty
+stabilise function element = Set.fromList (stabiliseList (Set.toList . function) element)
 
 --takes the epsilon closure of a state under the given transition function
 epsilonClosure :: (Ord states) => (Augmented alphabet -> states -> Set.Set states) -> states -> Set.Set states
 epsilonClosure transition = stabilise (transition Epsilon)
-
---unfold :: (b -> Maybe (a, b)) -> b -> [a]
-
 
 main :: IO()
 main = print (consecutivePairs ["test1", "test2"])
